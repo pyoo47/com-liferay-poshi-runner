94b7643a2652f2f6db65b74d1463be4cc0e30f2f
yichenroy <yi-chen.tsai@liferay.com>
2017-11-22 10:02:37 -0800

    LRQA-36161 Account for namespace in validateTestName

--- poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerValidation.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerValidation.java
@@ -1615,7 +1615,31 @@ public class PoshiRunnerValidation {
 		String className =
 			PoshiRunnerGetterUtil.getClassNameFromClassCommandName(testName);
 
-		if (!PoshiRunnerContext.isRootElement("test-case", className)) {
+		String classCommandName =
+			PoshiRunnerGetterUtil.getSimpleClassCommandName(testName);
+
+		String namespace =
+			PoshiRunnerGetterUtil.getNamespaceFromClassCommandName(testName);
+
+		boolean noRootElement;
+		boolean noCommandElement;
+
+		if (Validator.isNotNull(namespace)) {
+			noRootElement = !PoshiRunnerContext.isRootElement(
+				"test-case", className, namespace);
+
+			noCommandElement = !PoshiRunnerContext.isCommandElement(
+				"test-case", classCommandName, namespace);
+		}
+		else {
+			noRootElement = !PoshiRunnerContext.isRootElement(
+				"test-case", className);
+
+			noCommandElement = !PoshiRunnerContext.isCommandElement(
+				"test-case", classCommandName);
+		}
+
+		if (noRootElement) {
 			_exceptions.add(
 				new Exception(
 					StringBundler.concat(
@@ -1623,7 +1647,7 @@ public class PoshiRunnerValidation {
 						filePathLineNumber)));
 		}
 		else if (testName.contains("#")) {
-			if (!PoshiRunnerContext.isCommandElement("test-case", testName)) {
+			if (noCommandElement) {
 				String commandName =
 					PoshiRunnerGetterUtil.getCommandNameFromClassCommandName(
 						testName);
