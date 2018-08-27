77be885eebc37a9ecbb2cdd1f7f3a4f9dfeab2e7
Leslie Wong <leslie.wong@liferay.com>
2018-08-27 13:17:55 -0700

    LRQA-43124 Return PoshiElement directly if detected

--- poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerGetterUtil.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerGetterUtil.java
@@ -17,7 +17,6 @@ package com.liferay.poshi.runner;
 import com.google.common.reflect.ClassPath;
 
 import com.liferay.poshi.runner.elements.PoshiElement;
-import com.liferay.poshi.runner.elements.PoshiNode;
 import com.liferay.poshi.runner.elements.PoshiNodeFactory;
 import com.liferay.poshi.runner.prose.PoshiProseDefinition;
 import com.liferay.poshi.runner.selenium.SeleniumUtil;
@@ -376,23 +375,19 @@ public class PoshiRunnerGetterUtil {
 		String fileContent = FileUtil.read(url);
 		String filePath = url.getFile();
 
+		if (!fileContent.contains("<definition") &&
+			(filePath.endsWith(".macro") || filePath.endsWith(".testcase"))) {
+
+			return (PoshiElement)PoshiNodeFactory.newPoshiNodeFromFile(
+				filePath);
+		}
+
 		if (filePath.endsWith(".prose")) {
 			PoshiProseDefinition poshiProseDefinition =
 				new PoshiProseDefinition(
 					getFileNameFromFilePath(filePath), fileContent);
 
 			fileContent = Dom4JUtil.format(poshiProseDefinition.toElement());
-		}
-
-		if (!fileContent.contains("<definition") &&
-			(filePath.endsWith(".macro") || filePath.endsWith(".testcase"))) {
-
-			PoshiNode<?, ?> poshiNode = PoshiNodeFactory.newPoshiNodeFromFile(
-				filePath);
-
-			if (poshiNode instanceof PoshiElement) {
-				fileContent = Dom4JUtil.format((PoshiElement)poshiNode);
-			}
 		}
 
 		BufferedReader bufferedReader = new BufferedReader(
