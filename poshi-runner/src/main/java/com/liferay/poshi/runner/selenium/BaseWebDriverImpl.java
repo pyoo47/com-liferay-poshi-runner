e60293e465681e9ebaaf6c01d8d69d16c420c196
Kenji Heigel <kenji.heigel@liferay.com>
2019-06-06 18:43:57 -0700

    LRCI-343 Fix file path for Windows only for uploading, not archiving

--- poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
@@ -3078,8 +3078,6 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 
 		filePath = LiferaySeleniumHelper.getSourceDirFilePath(filePath);
 
-		filePath = FileUtil.fixFilePath(filePath);
-
 		if (value.endsWith(".jar") || value.endsWith(".war") ||
 			value.endsWith(".zip")) {
 
@@ -3096,6 +3094,8 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 				filePath = archiveFilePath;
 			}
 		}
+
+		filePath = FileUtil.fixFilePath(filePath);
 
 		uploadFile(location, filePath);
 	}
